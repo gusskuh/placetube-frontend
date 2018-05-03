96417a57c688963140ad1c326b2f1c5a57e2beb9
Amit Livne <amitush.l@gmail.com>
2018-05-03 15:39:44 +0300

    login in functions

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -1,33 +1,33 @@
-// import UserService from '../services/UserService.js'
-
+import UserService from '../services/UserService.js'
 
 export default {
-    // state: {
-    //     loggedinUser: null
-    //   },
-    //   mutations: {
-    //     setUser(state, {user}) {
-    //       state.loggedinUser = user;
-    //     }
-    //   },
-    //   getters: {
-    //     loggedinUser(state) {
-    //       return state.loggedinUser;
-    //     }
-    //   },
-    //   actions: {
-    //     login(store, {userCredentials}) {
-    //       return UserService.login(userCredentials)
-    //       .then(user=>{
-    //         store.commit({type: 'setUser', user});
-    //       })
-    //     },
-    //     logout(store) {
-    //         console.log(store);
-    //         return UserService.logout()
-    //         .then(()=>{
-    //           store.commit({type: 'setUser', user: null});
-    //         })
-    //       }
-    //   }
+    state: {
+        loggedinUser: null
+      },
+      mutations: {
+        setUser(state, {user}) {
+          state.loggedinUser = user;
+        }
+      },
+      getters: {
+        loggedinUser(state) {
+          return state.loggedinUser;
+        }
+      },
+      actions: {
+        login(store, {userCredentials}) {
+            
+          return UserService.login({email: userCredentials.email, password: userCredentials.password })
+          .then(user=>{
+            store.commit({type: 'setUser', user});
+          })
+        },
+        logout(store) {
+            console.log(store);
+            return UserService.logout()
+            .then(()=>{
+              store.commit({type: 'setUser', user: null});
+            })
+          }
+      }
 }
