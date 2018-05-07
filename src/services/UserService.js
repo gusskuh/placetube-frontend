d0f77014cabaaebcd12b284f56c9bbdc55f86e8b
oded levy <odedline@hotmail.com>
2018-05-07 14:46:31 +0300

    cloudinary added!

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,25 +1,28 @@
-const BASE_URL = 'http://127.0.0.1:3000';
-import axios from 'axios'
+const BASE_URL = "http://127.0.0.1:3000";
+import axios from "axios";
 
 function login(user) {
-
   return axios
     .post(`${BASE_URL}/login`, user)
     .then(res => {
       console.log(res.data.user);
-      
+
       localStorage.User = JSON.stringify(res.data.user);
 
-      return res.data.user
+      return res.data.user;
     })
-    .catch(err => {throw new Error('Login Failed')})
+    .catch(err => {
+      throw new Error("Login Failed");
+    });
 }
 
 function register(user) {
   return axios
     .post(`${BASE_URL}/register`, user)
     .then(res => console.log(res.data))
-    .catch(err => {throw new Error('Register Failed')})
+    .catch(err => {
+      throw new Error("Register Failed");
+    });
 }
 
 function logout() {
@@ -27,14 +30,33 @@ function logout() {
     .post(`${BASE_URL}/logout`)
     .then(res => {
       // localStorage.User = null;
-      localStorage.removeItem('User')
+      localStorage.removeItem("User");
       delete sessionStorage.user;
     })
-    .catch(err => {throw new Error('Logout Failed')})
+    .catch(err => {
+      throw new Error("Logout Failed");
+    });
+}
+
+function updateUser(user, newUrl) {
+  user.profileImg = newUrl;
+  console.log('update user from service ',user);
+  console.log('update newUrl from service ',newUrl);
+  console.log('update user._id from service ',user._id);
+  
+  return axios
+  
+  
+    .put(`${BASE_URL}/${user._id}`, user)
+    .then(res => {})
+    .catch(err => {
+      throw new Error("image update failed!");
+    });
 }
 
 export default {
   login,
   register,
-  logout
+  logout,
+  updateUser
 };
--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -1,33 +1,45 @@
-import UserService from '../services/UserService.js'
+import UserService from "../services/UserService.js";
 
 export default {
-    state: {
-        loggedinUser: null
-      },
-      mutations: {
-        setUser(state, {user}) {
-          state.loggedinUser = user;
-        },
-      },
-      getters: {
-        loggedinUser(state) {
-          return state.loggedinUser;
-        }
-      },
-      actions: {
-        login(store, {userCredentials}) {
-            
-          return UserService.login({email: userCredentials.email, password: userCredentials.password })
-          .then(user=>{
-            store.commit({type: 'setUser', user});
-          })
-        },
-        logout(store) {
-            console.log(store);
-            return UserService.logout()
-            .then(()=>{
-              store.commit({type: 'setUser', user: null});
-            })
-          }
-      }
-}
+  state: {
+    loggedinUser: null
+  },
+  mutations: {
+    setUser(state, { user }) {
+      state.loggedinUser = user;
+    },
+    updateUser(state, {user}) {
+      state.loggedinUser = user;
+    }
+  },
+  getters: {
+    loggedinUser(state) {
+      return state.loggedinUser;
+    }
+  },
+  actions: {
+    login(store, { userCredentials }) {
+      return UserService.login({
+        email: userCredentials.email,
+        password: userCredentials.password
+      }).then(user => {
+        store.commit({ type: "setUser", user });
+      });
+    },
+    logout(store) {
+      console.log(store);
+      return UserService.logout().then(() => {
+        store.commit({ type: "setUser", user: null });
+      });
+    },
+
+    updateUser(store, { user, newUrl }) {
+      user = JSON.parse(JSON.stringify(user))
+      user.profileImg = newUrl;
+      // console.log('&&&&&&&&&&&&&&&&&',user, newUrl);
+      UserService.updateUser(user, newUrl).then(() => {
+        store.commit({ type: "updateUser", user });
+      })
+    }
+  }
+};
